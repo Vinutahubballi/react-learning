f399bef8c01efb5e736398433fcf07f2e97421ab
Vinutahubballi <starvinuta@gmail.com>
2020-03-27 17:38:37 +0530

    Destructuring Props
    Do you want to know how to use props in Class and functional component? check in this commit.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./styles.css";
-import Greet from "./components/Greet.js";
+//import Greet from "./components/Greet.js";
 //import Welcome from "./components/Welcome.js";
 //import Message from "./components/message.js";
-import Counter from "./components/counter.js";
+//import Counter from "./components/counter.js";
+import Greet from "./components/heronames.js";
+import Heronames from "./components/Heronames.js";
 
 export default function App() {
   return (
     <div className="App">
-      <Greet />
-      <Counter />
+      <Greet name="BruceWyne" heronames="Batman" />
+      <Heronames name="Bruce" heronames="Hulk" />
     </div>
   );
 }
