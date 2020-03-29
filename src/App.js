a44661d341129370d2a9911c6ff619e0a0d8d9ca
Vinutahubballi <starvinuta@gmail.com>
2020-03-29 13:05:48 +0530

    Event handling and Event binding

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./styles.css";
 //import Welcome from "./components/Welcome.js";
 //import Message from "./components/message.js";
 //import Counter from "./components/counter.js";
-import Greet from "./components/heronames.js";
-import Heronames from "./components/Heronames.js";
-
+//import Greet from "./components/heronames.js";
+//import Heronames from "./components/Heronames.js";
+//import ClickClass from "./components/Eventhandleing.js";
+//import ClickComponent from "./components/FunctionalEventHandleing.js";
+import EventBinding from "./components/EventBinding.js";
 export default function App() {
   return (
     <div className="App">
-      <Greet name="BruceWyne" heronames="Batman" />
-      <Heronames name="Bruce" heronames="Hulk" />
+      <EventBinding />
     </div>
   );
 }
